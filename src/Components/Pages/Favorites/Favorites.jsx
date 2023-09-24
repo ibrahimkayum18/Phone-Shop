df243eca03c33699d3abb7e459f292f598bd8d37
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2023-09-24 23:37:58 +0600

    All Section Added

--- src/Components/Pages/Favorites/Favorites.jsx
+++ src/Components/Pages/Favorites/Favorites.jsx
@@ -1,46 +1,72 @@
 import { useEffect, useState } from "react";
 import FavoritePhones from "./FavoritePhone/FavoritePhones";
 
-
 const Favorites = () => {
-    const [newPhone, setNewPhone] = useState([]);
-    const [noData, setNoData] = useState('');
-    useEffect(() => {
-        const favorites = JSON.parse(localStorage.getItem("favorite"));
-        if (favorites) {
-            setNewPhone(favorites);
-        }else{
-            setNoData('No Data Found');
-        }
-    },[])
-    const handleDelete = () => {
-        localStorage.clear();
-        setNewPhone([])
-        setNoData('No Data Found')
+  const [newPhone, setNewPhone] = useState([]);
+  const [noData, setNoData] = useState("");
+  const [isShow, setIsShow] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favorite"));
+    if (favorites) {
+      const total = newPhone.reduce(
+        (previous, current) => previous + current.price,
+        0
+      );
+      setTotalPrice(total);
+      setNewPhone(favorites);
+    } else {
+      setNoData("No Data Found");
     }
-    console.log(newPhone);
-    return (
-        <div className="">
-            <div className="text-center">
-                {
-                    noData? <p className="text-1xl font-bold text-gray-500 mt-64 ">{noData}!!</p> : <div></div>
-                }
-            </div>
-            <div className="text-center my-5">
-            {
-                newPhone.length > 0 && <button onClick={handleDelete} className="btn bg-green-300">Delete All Favorites</button>
-            }
-            </div>
-            <div className="grid md:grid-cols-2 gap-5 mt-14">
-            {
-                newPhone.map(phone => <FavoritePhones 
-                key={phone.id}
-                phone={phone}
-                ></FavoritePhones>)
-            }
-            </div>
-        </div>
-    );
+  }, [newPhone]);
+  const handleDelete = () => {
+    localStorage.clear();
+    setNewPhone([]);
+    setNoData("No Data Found");
+  };
+  console.log(newPhone);
+  return (
+    <div className="">
+      <div className="text-center">
+        {noData ? (
+          <p className="text-1xl font-bold text-gray-500 mt-64 ">{noData}!!</p>
+        ) : (
+          <div></div>
+        )}
+      </div>
+      <div className="text-center my-5">
+        {newPhone.length > 0 && (
+          <div>
+            <button onClick={handleDelete} className="btn bg-green-300">
+              Delete All Favorites
+            </button>
+            <h2 className="text-2xl mt-4">Total Price: ${totalPrice}</h2>
+          </div>
+        )}
+      </div>
+      <div className="grid md:grid-cols-2 gap-5 mt-14">
+        {isShow
+          ? newPhone.map((phone) => (
+              <FavoritePhones key={phone.id} phone={phone}></FavoritePhones>
+            ))
+          : newPhone
+              .slice(0, 4)
+              .map((phone) => (
+                <FavoritePhones key={phone.id} phone={phone}></FavoritePhones>
+              ))}
+      </div>
+      <div className="text-center my-5">
+        {newPhone.length > 4 && (
+          <button
+            onClick={() => setIsShow(!isShow)}
+            className="btn bg-green-300"
+          >
+            {isShow ? "Show Less" : "See More"}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
--- src/Components/Phone/Phone.jsx
+++ src/Components/Phone/Phone.jsx
@@ -1,7 +1,8 @@
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
 const Phone = ({ phone }) => {
-  const {id, image, phone_name, brand_name, rating, price } = phone || {};
+  const { id, image, phone_name, brand_name, rating, price } = phone || {};
   return (
     <div className=" border-2 rounded-md p-5 mx-5 lg:mx-0">
       <img className="w-[80%] mx-auto" src={image} alt="" />
@@ -10,8 +11,7 @@ const Phone = ({ phone }) => {
         <h2 className="text-2xl text-gray-400 font-bold">{phone_name}</h2>
         <h2 className="text-2xl text-gray-400 font-bold">${price}</h2>
       </div>
-      <p className="font-bold text-gray-400">{rating}</p>
-
+      <Rating initialRating={rating} readonly />
       <div className="text-center mt-5">
         <Link to={`/details/${id}`}>
           <button className="btn btn-primary">View Details</button>
